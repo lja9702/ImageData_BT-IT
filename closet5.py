7eceeb08c58d26b047fb7f67762bebda7a3326e5
jykathar <42198329+jykathar@users.noreply.github.com>
2018-08-28 23:33:53 +0900

    GUI수정

--- closet5.py
+++ closet5.py
@@ -5,42 +5,47 @@
 # Created by: PyQt5 UI code generator 5.11.2
 #
 # WARNING! All changes made in this file will be lost!
+import sip
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap, QStandardItem
+from PyQt5.QtGui import QIcon, QPixmap, QStandardItem, QDragEnterEvent, QDropEvent
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QWidget, QVBoxLayout
 from PyQt5.QtGui import QIcon
+from colormap import rgb2hex
 import os
 from bs4 import BeautifulSoup
 import urllib.request
 import os
 import random
+
 import threading
 from selenium import webdriver
-from PyQt5.QtGui import QDragEnterEvent
-from PyQt5.QtGui import QDropEvent
 
 
 import Crolling_BrandWeb
 
-PATH = "/home/lja97/Crolling/"
+PATH = "C:\\Users/jykatharGram/Desktop/contest2/Brand_img/"
+global new
+new = ""
 
-Crolling_BrandWeb.get_Topten(url="https://www.topten10.co.kr/main/main.asp", brand="Topten")
-Crolling_BrandWeb.get_Mixxo(
-    url="http://mixxo.elandmall.com/main/initMain.action?chnl_no=GAW&chnl_dtl_no=1803401340&_emk_keyword=MIXXO&gclid=CjwKCAjw8O7bBRB0EiwAfbrTh68VSa9JCwk3GQWI-NjCHyojwrkavrow95BQjUkifKMPFftcWZCmzRoC59EQAvD_BwE&utm_referrer=http%3A%2F%2Fwww.elandmall.com%2Fgate%2Fgate.action%3Fchnl_no%3DGAW%26chnl_dtl_no%3D1803401340%26_emk_keyword%3DMIXXO%26gclid%3DCjwKCAjw8O7bBRB0EiwAfbrTh68VSa9JCwk3GQWI-NjCHyojwrkavrow95BQjUkifKMPFftcWZCmzRoC59EQAvD_BwE",
-    brand="Mixxo")
-Crolling_BrandWeb.get_PlasticIsland(url="https://www.theamall.com", brand="PlasticIsland")
-Crolling_BrandWeb.get_LuckyChouette(url="https://www.kolonmall.com/LUCKYCHOUETTE", brand="LuckyChouette")
-Crolling_BrandWeb.get_Tomboy(url="http://fashion.sivillage.com/display/brandTOMBOYMain?temp=www.tomboy.co.kr", brand="Tomboy")
-Crolling_BrandWeb.get_Spao(url="http://spao.elandmall.com/main/initMain.action", brand="Spao")
-Crolling_BrandWeb.get_8seconds(
-    url="http://www.ssfshop.com/8Seconds/main?dspCtgryNo=&brandShopNo=BDMA07A01&brndShopId=8SBSS&leftBrandNM=",
-    brand="8seconds")
-Crolling_BrandWeb.get_ALAND(url="http://www.a-land.co.kr", brand="ALAND")
+
+
+#Crolling_BrandWeb.get_Topten(url="https://www.topten10.co.kr/main/main.asp", brand="Topten")
+#Crolling_BrandWeb.get_Mixxo(
+#    url="http://mixxo.elandmall.com/main/initMain.action?chnl_no=GAW&chnl_dtl_no=1803401340&_emk_keyword=MIXXO&gclid=CjwKCAjw8O7bBRB0EiwAfbrTh68VSa9JCwk3GQWI-NjCHyojwrkavrow95BQjUkifKMPFftcWZCmzRoC59EQAvD_BwE&utm_referrer=http%3A%2F%2Fwww.elandmall.com%2Fgate%2Fgate.action%3Fchnl_no%3DGAW%26chnl_dtl_no%3D1803401340%26_emk_keyword%3DMIXXO%26gclid%3DCjwKCAjw8O7bBRB0EiwAfbrTh68VSa9JCwk3GQWI-NjCHyojwrkavrow95BQjUkifKMPFftcWZCmzRoC59EQAvD_BwE",
+#    brand="Mixxo")
+#Crolling_BrandWeb.get_PlasticIsland(url="https://www.theamall.com", brand="PlasticIsland")
+#Crolling_BrandWeb.get_LuckyChouette(url="https://www.kolonmall.com/LUCKYCHOUETTE", brand="LuckyChouette")
+#Crolling_BrandWeb.get_Tomboy(url="http://fashion.sivillage.com/display/brandTOMBOYMain?temp=www.tomboy.co.kr", brand="Tomboy")
+#Crolling_BrandWeb.get_Spao(url="http://spao.elandmall.com/main/initMain.action", brand="Spao")
+#Crolling_BrandWeb.get_8seconds(
+#    url="http://www.ssfshop.com/8Seconds/main?dspCtgryNo=&brandShopNo=BDMA07A01&brndShopId=8SBSS&leftBrandNM=",
+#    brand="8seconds")
+#Crolling_BrandWeb.get_ALAND(url="http://www.a-land.co.kr", brand="ALAND")
 
 class Banner(QWidget):
     def __init__(self, parent = None):
@@ -69,42 +74,43 @@ class Banner(QWidget):
         bannerLayout.addWidget(self.Banner_img)
         bannerLayout.addWidget(self.Banner_name)
 
-        #threading.Timer(3, self.__init__).start()
-
-
-
-
 
 
 class closetLabel(QLabel):
     def __init__(self, title):
         QLabel.__init__(self, title)
-        self.setText("<옷장에서 옷을 드래그 해주세요>")
+        self.setText("<옷을 추가해주세요>")
         self.setAcceptDrops(True)
+        self.check = 0
 
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls:
             event.accept()
+            self.check = 0
         else:
             event.ignore()
 
     def dragMoveEvent(self, event):
-     if event.mimeData().hasUrls:
-         event.setDropAction(QtCore.Qt.CopyAction)
-         event.accept()
-     else:
-         event.ignore()
+        if event.mimeData().hasUrls:
+            event.setDropAction(QtCore.Qt.CopyAction)
+            event.accept()
+            self.check = 0
+        else:
+            event.ignore()
 
     def dropEvent(self, event):
         if event.mimeData().hasUrls:
             event.setDropAction(QtCore.Qt.CopyAction)
             event.accept()
             for url in event.mimeData().urls():
-                path = str(url.toLocalFile())
-                closetPixelMap = QPixmap(path)
+                self.path = str(url.toLocalFile())
+                closetPixelMap = QPixmap(self.path)
                 smaller_pixmap = closetPixelMap.scaled(431, 431, Qt.KeepAspectRatio, Qt.FastTransformation)
                 self.setPixmap(smaller_pixmap)
+                self.check = 1
+
+
 
 
         else:
@@ -143,44 +149,9 @@ class myCloset(QWidget):
         path = self.dirModel.fileInfo(index).absoluteFilePath()
         self.listview.setRootIndex(self.fileModel.setRootPath(path))
 
-
-class valueDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.setupUI()
-
-        self.cost = None
-        self.brand = None
-
-    def setupUI(self):
-        self.setGeometry(1100, 200, 300, 100)
-        self.setWindowTitle("Cost & Brand")
-        self.setWindowIcon(QIcon('icon.png'))
-
-        label1 = QLabel("Cost: ")
-        label2 = QLabel("Brand: ")
-
-        self.lineEdit1 = QLineEdit()
-        self.lineEdit2 = QLineEdit()
-        self.pushButton1= QPushButton("OK")
-        self.pushButton1.clicked.connect(self.pushButtonClicked)
-
-        layout = QGridLayout()
-        layout.addWidget(label1, 0, 0)
-        layout.addWidget(self.lineEdit1, 0, 1)
-        layout.addWidget(self.pushButton1, 0, 2)
-        layout.addWidget(label2, 1, 0)
-        layout.addWidget(self.lineEdit2, 1, 1)
-
-        self.setLayout(layout)
-
-    def pushButtonClicked(self):
-        self.cost = self.lineEdit1.text()
-        self.brand = self.lineEdit2.text()
-        self.close()
-
-
 class Ui_MainWindow(object):
+
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("옷장 관리")
         MainWindow.resize(1291, 848)
@@ -207,11 +178,7 @@ class Ui_MainWindow(object):
         self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget_2)
         self.pushButton_2.setObjectName("pushButton_2")
         self.verticalLayout_2.addWidget(self.pushButton_2)
-        #self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
-        #self.label_2.setObjectName("label_2")
-        #self.verticalLayout_2.addWidget(self.label_2)
-        cl = closetLabel("closetLabel")
-        self.verticalLayout_2.addWidget(cl)
+
         self.verticalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(860, 0, 431, 431))
         self.verticalLayoutWidget_3.setObjectName("verticalLayoutWidget_3")
@@ -222,8 +189,13 @@ class Ui_MainWindow(object):
         self.pushButton_3.setObjectName("pushButton_3")
         self.verticalLayout_3.addWidget(self.pushButton_3)
         self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget_3)
-        self.label_3.setObjectName("label_3")
-        self.verticalLayout_3.addWidget(self.label_3)
+        #self.label_3.setObjectName("label_3")
+        #self.label_3.setAcceptDrops(True)
+        #self.verticalLayout_3.addWidget(self.label_3)
+
+        self.cl = closetLabel("closetLabel")
+        self.cl.acceptDrops()
+        self.verticalLayout_3.addWidget(self.cl)
         self.verticalLayoutWidget_4 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_4.setGeometry(QtCore.QRect(430, 430, 431, 181))
         self.verticalLayoutWidget_4.setObjectName("verticalLayoutWidget_4")
@@ -242,12 +214,12 @@ class Ui_MainWindow(object):
         self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_5)
         self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout_5.setObjectName("verticalLayout_5")
-        self.pushButton_6 = QtWidgets.QPushButton(self.verticalLayoutWidget_5)
-        self.pushButton_6.setObjectName("pushButton_6")
-        self.verticalLayout_5.addWidget(self.pushButton_6)
-        self.pushButton_7 = QtWidgets.QPushButton(self.verticalLayoutWidget_5)
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.verticalLayout_5.addWidget(self.pushButton_7)
+        #self.pushButton_6 = QtWidgets.QPushButton(self.verticalLayoutWidget_5)
+        #self.pushButton_6.setObjectName("pushButton_6")
+        #self.verticalLayout_5.addWidget(self.pushButton_6)
+        #self.pushButton_7 = QtWidgets.QPushButton(self.verticalLayoutWidget_5)
+        #self.pushButton_7.setObjectName("pushButton_7")
+        #self.verticalLayout_5.addWidget(self.pushButton_7)
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 610, 431, 181))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
@@ -277,31 +249,129 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         self.pushButton_3.clicked.connect(self.openFile)
-        self.pushButton_6.clicked.connect(self.inputValue)
+
+        self.info_layout = QGridLayout()
+        self.info_button = QPushButton("옷 정보보기")
+        self.info_button.clicked.connect(self.infoButtonClicked)
+        self.info_layout.addWidget(self.info_button)
+        self.verticalLayout_5.addLayout(self.info_layout)
+
+
+    def infoButtonClicked(self):
+        if self.cl.check == 0:
+            for i in reversed(range(self.info_layout.count())):
+                self.info_layout.itemAt(i).widget().deleteLater()
+            self.label1 = QLabel("Type: ")
+            self.label2 = QLabel("Cost: ")
+            self.label3 = QLabel("Brand: ")
+            self.label4 = QLabel("Color: ")
+            fn = os.path.split(self.new_cloth_name)
+            self.label5 = QLabel(fn[1])
+
+            self.lineEdit1 = QLineEdit()
+            self.lineEdit2 = QLineEdit()
+            self.styleChoice = QtWidgets.QLabel()
+            self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % (rgb2hex(0, 128, 64)))
+            self.fontColor = QtWidgets.QPushButton('Changing Color')
+            self.fontColor.clicked.connect(self.color_picker)
+            self.pushButton1 = QPushButton("내 옷장에 추가하기")
+            self.pushButton1.clicked.connect(self.OKButtonClicked)
+            self.pushButton2 = QPushButton("뒤로가기")
+            self.pushButton2.clicked.connect(self.backButtonClicked)
+
+            self.info_layout.addWidget(self.label1, 0, 0)
+            self.info_layout.addWidget(self.label2, 1, 0)
+            self.info_layout.addWidget(self.label3, 2, 0)
+            self.info_layout.addWidget(self.label4, 3, 0)
+            self.info_layout.addWidget(self.label5, 0, 1)
+            self.info_layout.addWidget(self.lineEdit1, 1, 1)
+            self.info_layout.addWidget(self.lineEdit2, 2, 1)
+            self.info_layout.addWidget(self.styleChoice, 3, 1)
+            self.info_layout.addWidget(self.fontColor, 3, 2)
+            self.info_layout.addWidget(self.pushButton1, 4, 1)
+            self.info_layout.addWidget(self.pushButton2, 4, 2)
+        else:
+            for i in reversed(range(self.info_layout.count())):
+                self.info_layout.itemAt(i).widget().deleteLater()
+            self.label1 = QLabel("Type: ")
+            self.label2 = QLabel("Cost: ")
+            self.label3 = QLabel("Brand: ")
+            self.label4 = QLabel("Color: ")
+            fn = os.path.split(self.cl.path)
+            self.label5 = QLabel(fn[1])
+            self.label6 = QLabel("내 옷 가격")
+            self.label7 = QLabel("내 옷 브랜드")
+            self.styleChoice = QtWidgets.QLabel()
+            self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % (rgb2hex(0, 128, 64)))
+            self.fontColor = QtWidgets.QPushButton('Changing Color')
+            self.fontColor.clicked.connect(self.color_picker)
+            self.pushButton2 = QPushButton("뒤로가기")
+            self.pushButton2.clicked.connect(self.backButtonClicked)
+
+            self.info_layout.addWidget(self.label1, 0, 0)
+            self.info_layout.addWidget(self.label2, 1, 0)
+            self.info_layout.addWidget(self.label3, 2, 0)
+            self.info_layout.addWidget(self.label4, 3, 0)
+            self.info_layout.addWidget(self.label5, 0, 1)
+            self.info_layout.addWidget(self.label6, 1, 1)
+            self.info_layout.addWidget(self.label7, 2, 1)
+            self.info_layout.addWidget(self.styleChoice, 3, 1)
+            self.info_layout.addWidget(self.fontColor, 3, 2)
+            self.info_layout.addWidget(self.pushButton2, 4, 2)
+            self.cl.check = 0
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "옷장 관리"))
         self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.label.setText(_translate("MainWindow", "<옷을 추가해주세요>"))
-        self.pushButton_2.setText(_translate("MainWindow", "내 옷장에서 불러오기"))
-        #self.label_2.setText(_translate("MainWindow", "<옷을 추가해주세요>"))
+        self.label.setText(_translate("MainWindow", "<옷 추천>"))
+        self.pushButton_2.setText(_translate("MainWindow", "매칭"))
         self.pushButton_3.setText(_translate("MainWindow", "옷 불러오기"))
-        self.label_3.setText(_translate("MainWindow", "<옷을 추가해주세요>"))
         self.pushButton_4.setText(_translate("MainWindow", "PushButton"))
         self.pushButton_5.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_6.setText(_translate("MainWindow", "PushButton"))
-        self.pushButton_7.setText(_translate("MainWindow", "PushButton"))
+        #self.pushButton_6.setText(_translate("MainWindow", "PushButton"))
+        #self.pushButton_7.setText(_translate("MainWindow", "PushButton"))
 
     def openFile(self):
-        fname = QtWidgets.QFileDialog.getOpenFileName(None, "Open Movie", QDir.homePath())
-        PixelMap = QPixmap(fname[0])
-        smaller_pixmap = PixelMap.scaled(431, 431, Qt.KeepAspectRatio, Qt.FastTransformation)
-        self.label_3.setPixmap(smaller_pixmap)
 
-    def inputValue(self):
-        dlg = valueDialog()
-        dlg.exec_()
+        fname = QtWidgets.QFileDialog.getOpenFileName(None, "Open Movie", QDir.homePath())
+        self.new_cloth_name = fname[0]
+        PixelMap = QPixmap(self.new_cloth_name)
+        smaller_pixmap = PixelMap.scaled(431, 431, Qt.KeepAspectRatio, Qt.FastTransformation)
+        self.cl.setPixmap(smaller_pixmap)
+
+        for i in reversed(range(self.info_layout.count())):
+            self.info_layout.itemAt(i).widget().deleteLater()
+
+        self.info_button = QPushButton("옷 정보보기")
+        self.info_button.clicked.connect(self.infoButtonClicked)
+        self.info_layout.addWidget(self.info_button)
+
+
+
+    def OKButtonClicked(self):
+        self.cost = self.lineEdit1.text()
+        self.brand = self.lineEdit2.text()
+        self.close()
+
+    def backButtonClicked(self):
+        for i in reversed(range(self.info_layout.count())):
+            self.info_layout.itemAt(i).widget().deleteLater()
+        self.info_button = QPushButton("옷 정보보기")
+        self.info_button.clicked.connect(self.infoButtonClicked)
+        self.info_layout.addWidget(self.info_button)
+
+    def color_picker(self):
+        color = QtWidgets.QColorDialog.getColor()
+        print(color.name())
+        self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % color.name())
+
+    def style_choice(self, text):
+        self.styleChoice.setText(text)
+        QtWidgets.QApplication.setStyle(QtWidgets.QStyleFactory.create(text))
+
+
+
 
 
 if __name__ == "__main__":
